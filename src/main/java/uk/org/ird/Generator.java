7ea3673410e8470151a6531f2e2ae3c81aaca3ac
ird <31051194+ird@users.noreply.github.com>
2017-12-16 15:53:36 +0000

    Generator: all input parser functions added, SolutionSpace: constructor and toString methods added

--- src/main/java/uk/org/ird/Generator.java
+++ src/main/java/uk/org/ird/Generator.java
@@ -1,29 +1,68 @@
 package uk.org.ird;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.charset.Charset;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+import static java.nio.file.StandardOpenOption.APPEND;
+import static java.nio.file.StandardOpenOption.WRITE;
 
 public class Generator {
 	public static ArrayList<String> buildArrayFromFile(String inputFile) throws IOException {
+		return buildArrayFromFileFiltered(inputFile, null);
+	}
+	public static ArrayList<String> buildArrayFromFileFiltered(String inputFile, ArrayList<String> filter) throws IOException {
 		ArrayList<String> res = new ArrayList<>();
 		Charset cs = Charset.forName("US-ASCII");
 		BufferedReader reader = Files.newBufferedReader(Paths.get(inputFile), cs);
 		String line = null;
 		while((line = reader.readLine()) != null) {
-			res.add(line);
+			if(filter != null) {
+				for(String f: filter) {
+					if(line.contains(f)){
+						line = line.replaceAll("[^a-zA-Z!0-9]", "").toUpperCase();
+						res.add(line);
+						continue;
+					}
+				}
+			} else {
+				line = line.replaceAll("[^a-zA-Z!0-9]", "").toUpperCase();
+				res.add(line);
+			}
 		}
 		return res;
 	}
+	public static void songBankPreprocess(String input, String output) throws IOException {
+		Charset cs = Charset.forName("Cp1252");
+		Charset cs_write = Charset.forName("US-ASCII");
+		OpenOption[] options = { CREATE_NEW, APPEND, WRITE };
+		BufferedReader reader = Files.newBufferedReader(Paths.get(input), cs);
+		BufferedWriter writer = Files.newBufferedWriter(Paths.get(output), cs_write, options);
+		String line = null;
+		while((line = reader.readLine()) != null) {
+			String[] parts = line.split("<SEP>");
+			if(parts.length < 4)
+				continue;
+			line = parts[2] + parts[3];
+			line = line.replaceAll("[^a-zA-Z!0-9]", "").toUpperCase();
+			writer.write(line, 0, line.length());
+			writer.newLine();
+		}
+	}
 	public static void main(String[] args) {
+		
 		ArrayList<String> dexterVictims;
 		ArrayList<String> songs;
 		try {
+			//songBankPreprocess("dex/song_titles.txt", "dex/songs.txt");
 			dexterVictims = buildArrayFromFile("dex/victims.txt");
-			songs = buildArrayFromFile("dex/songs.txt");
+			songs = buildArrayFromFileFiltered("dex/songs.txt", dexterVictims);
 		} catch (IOException x) {
 			System.err.format("IOException: %s%n", x);
 			return;
@@ -34,7 +73,7 @@ public class Generator {
 		for(String s: songs) {
 			System.out.println(s);
 		}
-		//Solution dexterWherePuzzle = new Solution(dexterVictims, songs);
-		//System.out.println(dexterWherePuzzle.solve());
+		SolutionSpace dexterWherePuzzle = new SolutionSpace(dexterVictims, songs);
+		System.out.println(dexterWherePuzzle);
 	}
 }
--- src/main/java/uk/org/ird/SolutionSpace.java
+++ src/main/java/uk/org/ird/SolutionSpace.java
@@ -8,13 +8,24 @@ public class SolutionSpace {
 	private LinkedList<String> answer;
 	private HashMap<Integer, LinkedList<String>> solutionMap;
 	/**
-	 *
-	 *
-	 *
+	 * Initialises a SolutionSpace and builds the solutionMap from two ArrayLists
+	 * @param orderList An array of items which dictate the order of the solution in the puzzle
+	 * @param inputStrings An array of strings which may be used to make the solution
 	 */
 	public SolutionSpace(ArrayList<String> orderList, ArrayList<String> inputStrings) {
 		answer = new LinkedList<>();
-		//build solution map
+		solutionMap = new HashMap<>();
+		int i = 0;
+		for(String item : orderList) {
+			LinkedList<String> matches = new LinkedList<>();
+			for(String input : inputStrings) {
+				if(input.contains(item)){
+					matches.add(input);
+				}
+			}
+			solutionMap.put(i, matches);
+			i++;	
+		}
 	}
 	/**
 	 *
@@ -31,5 +42,13 @@ public class SolutionSpace {
 	 */
 	public boolean findAnswer(String answer, int col) {
 		return false;
+	}
+	public String toString() {
+		String res = new String();
+		for(Integer k : solutionMap.keySet()) {
+			res += "Posn " + k.toString() + ": \n";
+			res += solutionMap.get(k).toString() + "\n";
+		}
+		return res;
 	}
 }
